e484080697c9b20b390de6936315961fa7b8d05a
Derek <48116904+SoLetsCode@users.noreply.github.com>
2020-03-28 11:51:18 -0700

    Updates

---
+++ precache-manifest.bf037e64c5bd8c0a540d06b2cb2b0dcd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57e6937a3713eeabc6c3719963ed599e",
+    "url": "/ResumeBuilder/index.html"
+  },
+  {
+    "revision": "58e8dd4948052153ad28",
+    "url": "/ResumeBuilder/static/css/main.a2a8d142.chunk.css"
+  },
+  {
+    "revision": "26c1e20bf6e63f5b8185",
+    "url": "/ResumeBuilder/static/js/2.9246e312.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ResumeBuilder/static/js/2.9246e312.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58e8dd4948052153ad28",
+    "url": "/ResumeBuilder/static/js/main.9fd18228.chunk.js"
+  },
+  {
+    "revision": "134383a55e2e14de0e7d",
+    "url": "/ResumeBuilder/static/js/runtime-main.9d5f2e34.js"
+  },
+  {
+    "revision": "d1276a492d248b2d465234804f4a878f",
+    "url": "/ResumeBuilder/static/media/cpu.d1276a49.svg"
+  }
+]);
